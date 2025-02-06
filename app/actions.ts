42b3ff3c30a89aec514a5022777813f5c5ec1204
Anton <gh@gets.love>
2025-02-06 02:15:40 +0100

    implement to/fromVault

--- app/actions.ts
+++ app/actions.ts
@@ -1,31 +1,57 @@
 "use server";
 
 import { fromBase64, toBase64 } from "@cosmjs/encoding";
+import { SecretVaultWrapper } from "nillion-sv-wrappers";
+import { orgConfig } from "./nillionOrgConfig";
 
 type Result<R> = Promise<
   { ok: true; value: R } | { ok: false; message: string }
 >;
 
-export const toVault: (
-  name: string,
-  data: string | Uint8Array,
-) => Result<void> = async (name, data) => {
-  try {
-    const _name = name;
-    const _encoded = toBase64(new Uint8Array(Buffer.from(data)));
+const DOCS: SecretVaultWrapper = await (async () => {
+  const collection = new SecretVaultWrapper(
+    orgConfig.nodes,
+    orgConfig.orgCredentials,
+    process.env["NOTARY_SECDOC_SCHEMA_ID"],
+  );
+  await collection.init();
+  return collection;
+})();
 
+export const toVault: (name: string, data: Uint8Array) => Result<void> = async (
+  name,
+  data,
+) => {
+  try {
+    // TODO split into N parts on 16M size boundary
+    const dataWritten = await DOCS.writeToNodes([
+      {
+        name: { $allot: name },
+        data: { $allot: toBase64(data) },
+      },
+    ]);
+
+    console.log("Data written to nodes:", JSON.stringify(dataWritten, null, 2));
+
+    const newIds = [
+      ...new Set(
+        dataWritten.map((item: any) => item.result.data.created).flat(),
+      ),
+    ];
+
+    console.log("Uploaded record IDs:", newIds);
     return { ok: true, value: undefined };
   } catch (error) {
     return { ok: false, message: `Server error: failed to upload: ${error}` };
   }
 };
 
-export const fromVault: (name: string) => Result<void> = async (name) => {
+export const fromVault: (name: string) => Result<Uint8Array> = async (name) => {
   try {
-    const _name = name;
-    const _decoded = fromBase64("");
-
-    return { ok: true, value: undefined };
+    const decryptedCollectionData = await DOCS.readFromNodes({ name });
+    console.log(`Records under ${name}:`);
+    decryptedCollectionData.forEach((v: any) => console.log(v));
+    return { ok: true, value: decryptedCollectionData[0] };
   } catch (error) {
     return { ok: false, message: `Server error: failed to upload: ${error}` };
   }
